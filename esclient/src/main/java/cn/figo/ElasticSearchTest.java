3b9d0bc75969553a016a2f6d95040860eb5fde3c
YourFigo <501385003@qq.com>
2019-12-25 23:00:35 +0800

    添加mappings信息

--- esclient/src/main/java/cn/figo/ElasticSearchTest.java
+++ esclient/src/main/java/cn/figo/ElasticSearchTest.java
@@ -3,6 +3,8 @@ package cn.figo;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.Test;
 
@@ -29,5 +31,75 @@ public class ElasticSearchTest {
         //4、关闭client对象
         client.close();
     }
+
+    @Test
+    public void setMappings() throws Exception{
+        Settings settings = Settings.builder().put("cluster.name","my-elasticsearch").build();
+        TransportClient client = new PreBuiltTransportClient(settings);
+        client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"),9300));
+        client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"),9301));
+        client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"),9302));
+        //创建一个Mappings信息
+        /*{
+            "article":{
+            "properties":{
+                "id":{
+                    "type":"long",
+                            "store":true
+                },
+                "title":{
+                    "type":"text",
+                            "store":true,
+                            "index":true,
+                            "analyzer":"ik_smart"
+                },
+                "content":{
+                    "type":"text",
+                            "store":true,
+                            "index":true,
+                            "analyzer":"ik_smart"
+                }
+            }
+        }
+        }*/
+
+        // 使用 XContentBuilder 描述json数据
+        XContentBuilder builder = XContentFactory.jsonBuilder()
+            .startObject()
+                .startObject("article")
+                    .startObject("properties")
+                        .startObject("id")
+                            .field("type","long")
+                            .field("store", true)
+                        .endObject()
+                        .startObject("title")
+                            .field("type", "text")
+                            .field("store", true)
+                            .field("analyzer", "ik_smart")
+                        .endObject()
+                        .startObject("content")
+                            .field("type", "text")
+                            .field("store", true)
+                            .field("analyzer","ik_smart")
+                        .endObject()
+                    .endObject()
+                .endObject()
+            .endObject();
+
+        //使用client把mapping信息设置到索引库中
+        client.admin().indices()
+                //设置要做映射的索引
+                .preparePutMapping("index_hello")
+                //设置要做映射的type
+                .setType("article")
+                //mapping信息，可以是XContentBuilder对象可以是json格式的字符串
+                .setSource(builder)
+                //执行操作
+                .get();
+        //关闭链接
+        client.close();
+    }
+
+
 
 }
